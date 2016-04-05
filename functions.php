6612e9da29c73e485fdfb8a980fbfc5c762dbef3
Çağatay ÇALI <me@calicagatay.me>
2016-04-05 23:02:35 +0300

    Bug fix7

--- functions.php
+++ functions.php
@@ -9,6 +9,19 @@
 class Functions
 {
 
+    function getUrlContent($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        $data = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return ($httpcode>=200 && $httpcode<300) ? $data : false;
+    }
+
     /**
      * @param $ilan_sayisi
      * @param $sonuc
@@ -182,7 +195,7 @@ class Functions
     {
         $result = array();
 
-        $site = file_get_contents($url);
+        $site = $this->getUrlContent($url);
 
         $result["url"]   = $url;
         $result["location"] = $this->getLocation($site,$latitude,$longitude);
@@ -221,7 +234,7 @@ class Functions
                 # Current url
                 $url ="http://www.sahibinden.com".$detay_icin_link[1][$j];
 
-                $sonuc[$j] = $functions->getDetail($url,$latitude,$longitude);
+                $sonuc[$j] = $this->getDetail($url,$latitude,$longitude);
             }
         }
 
--- index.php
+++ index.php
@@ -3,18 +3,7 @@ header('Access-Control-Allow-Origin: *');
 require 'vendor/autoload.php';
 require 'functions.php';
 
-function getUrlContent($url){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    $data = curl_exec($ch);
-    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    return ($httpcode>=200 && $httpcode<300) ? $data : false;
-}
+
 
 Flight::route('GET /', function(){
     echo 'Hoşgeldin, get isteği kabul etmiyoruz lütfen konum bilgilerini post ile gönder!';
@@ -62,7 +51,7 @@ Flight::route('POST /', function(){
     {
         $url = "http://www.sahibinden.com/emlak-konut?pagingSize=50&pagingOffset=$i&query_text=".$street;
 
-        $url = getUrlContent($url);
+        $url = $functions->getUrlContent($url);
 
         //echo "<a href='$url'>$url</a><br>";
 
