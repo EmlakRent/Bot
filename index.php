c34c40216eb89bef3f96e51d7ef9cdafa4af9fa5
Çağatay ÇALI <me@calicagatay.me>
2016-04-05 22:59:33 +0300

    Bug fix6

--- index.php
+++ index.php
@@ -3,7 +3,18 @@ header('Access-Control-Allow-Origin: *');
 require 'vendor/autoload.php';
 require 'functions.php';
 
-
+function getUrlContent($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    $data = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return ($httpcode>=200 && $httpcode<300) ? $data : false;
+}
 
 Flight::route('GET /', function(){
     echo 'Hoşgeldin, get isteği kabul etmiyoruz lütfen konum bilgilerini post ile gönder!';
@@ -49,7 +60,9 @@ Flight::route('POST /', function(){
 # todo : mbdef class'ına sahip divi bul ve kaç sayfa olduğunu tespit et && infoSearchResults
     for ( $i = 0 ; $i < 50 ; $i = $i + 50)
     {
-        $url = file_get_contents("http://www.sahibinden.com/emlak-konut?pagingSize=50&pagingOffset=$i&query_text=".$street);
+        $url = "http://www.sahibinden.com/emlak-konut?pagingSize=50&pagingOffset=$i&query_text=".$street;
+
+        $url = getUrlContent($url);
 
         //echo "<a href='$url'>$url</a><br>";
 
