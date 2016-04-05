62aa25aab00064816afdcbf767c310e8a158e921
Çağatay ÇALI <me@calicagatay.me>
2016-04-05 23:22:32 +0300

    Bug fixed4

--- functions.php
+++ functions.php
@@ -217,7 +217,7 @@ class Functions
 
         $result["url"]   = $url;
         $result["location"] = $this->getLocation($site,$latitude,$longitude);
-        $result["price"]  = $this->getPrice($site);
+        //$result["price"]  = $this->getPrice($site);
 
         return $result;
     }
